f64b448bde81e4d406bd76fde5c36719de3a6f0d
Maria Strangas <mariastrangas@gmail.com>
2016-06-01 14:02:28 +1000

    new lesson on merging data frames

--- CtoF.py
+++ CtoF.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-
-import sys
+from sys import argv
 
 def celsius_to_fahr(temp_c):
     temp_f =(temp_c * (9.0/5.0)) + 32 
     return temp_f
 
-try:
-	celc =float(sys.argv[1])
-	print celsius_to_fahr(celc)
+def dowork():
+	try:
+		celc =float(argv[1])
+		print celsius_to_fahr(celc)
 
-except:
-	print "no good, try again"
+	except:
+		print "no good, try again"
 
 
+if __name__ == "__main__":
+	dowork()
