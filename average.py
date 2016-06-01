ed65d87a59cc5a2434088cbbf17da3178f3afac7
Maria Strangas <mariastrangas@gmail.com>
2016-06-01 16:44:36 +1000

    finished mean-by-group exercise

--- average.py
+++ average.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 
+import sys
+import pandas as pd
+
 def main():
-	print yo yoyo yo
+	filename = sys.argv[1]
+	year = int(sys.argv[2])
+	factor = sys.argv[3]
 
+	survey_df = pd.read_csv(filename)
+	sub_year = survey_df[survey_df.year == year]
+	grouping_df = sub_year.groupby(factor)['weight']
+	mean_by_group = grouping_df.mean()
 
-
+	print mean_by_group
 
 if __name__ == "__main__":
-	dowork()
+	main()
